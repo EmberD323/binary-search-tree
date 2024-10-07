7162268b3b0d97d757f185f681ea842350d87421
Ember Doyle <doyle.ember@gmail.com>
2024-10-07 09:09:16 -0600

    Create test envioronment

--- src/script.js
+++ src/script.js
@@ -1,19 +1,47 @@
 import {Tree} from "./binarysearchtree.js"
-let test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let numbers = [];
 
-let tree = new Tree(test);
-
-
+//66 randomn number
+for(let i=0;i<66;i++){
+    numbers.push(getRandom())
+}
+//create tree
+let tree = new Tree(numbers);
 console.log(tree);
-tree.insert(122);
-tree.insert(132);
 
-console.log(tree.isBalanced())
+//confirm balanced
+console.log(tree.isBalanced());
+
+//level,pre,post,inorder
+tree.levelOrder(print)
+tree.preOrder(print);
+tree.postOrder(print);
+tree.inOrder(print);
+
+//unbalance by adding 7 numbers
+tree.insert(99);
+tree.insert(98);
+tree.insert(97);
+tree.insert(96);
+tree.insert(95);
+tree.insert(94);
+tree.insert(93);
+console.log(tree.isBalanced());
+//rebalance
 tree.rebalance();
-console.log(tree)
-// tree.postOrder(print);
-// function print(node){
-//     console.log(node)
-// }
+console.log(tree.isBalanced());
+
+//level,pre,post,inorder
+tree.levelOrder(print)
+tree.preOrder(print);
+tree.postOrder(print);
+tree.inOrder(print);
+
+function getRandom() {
+    return Math.round(Math.random()*100);
+  }
+function print(node){
+     console.log(node)
+}
 
 
